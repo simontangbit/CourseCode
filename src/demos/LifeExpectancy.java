c2259de31a62e0e08eef381ee78f1b526cd6a3bf
Simon Tang <tangshiping@gmail.com>
2017-09-24 03:09:49 +0800

    加入第3讲课堂练习代码

---
+++ src/demos/LifeExpectancy.java
@@ -0,0 +1,54 @@
+package demos;
+
+import processing.core.PApplet;
+import de.fhpotsdam.unfolding.UnfoldingMap;
+import de.fhpotsdam.unfolding.utils.MapUtils;
+import de.fhpotsdam.unfolding.providers.*;
+import de.fhpotsdam.unfolding.providers.Google.*;
+
+import java.util.List;
+import de.fhpotsdam.unfolding.data.Feature;
+import de.fhpotsdam.unfolding.data.GeoJSONReader;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+import de.fhpotsdam.unfolding.marker.Marker;
+
+/**
+ * Visualizes life expectancy in different countries. 
+ * 
+ * It loads the country shapes from a GeoJSON file via a data reader, and loads the population density values from
+ * another CSV file (provided by the World Bank). The data value is encoded to transparency via a simplistic linear
+ * mapping.
+ */
+public class LifeExpectancy extends PApplet {
+
+	UnfoldingMap map;
+	Map<String, Float> lifeExpByCountry;
+	List<Feature> countries;
+	List<Marker> countryMarkers;
+
+	public void setup() {
+		
+	}
+
+	public void draw() {
+		
+	}
+
+	//Helper method to color each country based on life expectancy
+	//Red-orange indicates low (near 40)
+	//Blue indicates high (near 100)
+	private void shadeCountries() {
+		
+	}
+
+	//Helper method to load life expectancy data from file
+	private Map<String, Float> loadLifeExpectancyFromCSV(String fileName) {
+		
+		return null;
+	}
+
+}
